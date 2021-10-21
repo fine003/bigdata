5c2da31f974369cf99ad936923b963dd54fa7df3
yunt <yunt2213930228@qq.com>
2021-10-21 16:56:09 +0800

    ieda 添加sou3

--- src/java/test/HelloWord.java
+++ src/java/test/HelloWord.java
@@ -3,6 +3,9 @@ package test;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("helloWord");
+        System.out.println("you are sb1");
+        System.out.println("you are sb2");
+        System.out.println("you are sb3");
 
     }
 }
